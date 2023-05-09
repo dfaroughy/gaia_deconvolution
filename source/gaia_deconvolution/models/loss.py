fixed issue with sub-batching and gradient accumulation

source/gaia_deconvolution/models/loss.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from copy import deepcopy
+from gaia_deconvolution.data.transform import GaiaTransform
 
 def calculate_loss(model, data, args, loss_func=None, reduction=torch.mean):
 	if not loss_func: loss_func = args.loss
@@ -14,27 +15,43 @@ def neglogprob_loss(model, batch, args):
 	loss = - model.log_prob(batch[:, :d])
 	return loss
 
-
 def deconv_loss(model, batch, args):
 	d = args.dim
 	n = args.num_mc
-	device = args.device
-	batch = batch.repeat_interleave(n, 0).to(device)                   # ABC... -> AABBCC...
-	x = batch[:, :d]													  # x = (x1, x2)
-	cov_flat = batch[:, d:] 											  # cov = (c11, c12, c21, c22)  flat
-	cov_matrix = torch.reshape(cov_flat, (-1, d, d)).to(device)         # cov = ((c11, c12),(c21, c22))  reshaped
+	batch = batch.repeat_interleave(n, 0)                   # ABC... -> AABBCC...
+	x = batch[:, :d]										# x = (x1, x2)
+	cov_flat = batch[:, d:] 								# cov = (c11, c12, c21, c22)  flat
+	cov_matrix = torch.reshape(cov_flat, (-1, d, d))        # cov = ((c11, c12),(c21, c22))  reshaped
 	eps = torch.randn_like(x)                                         # sample eps ~ N(0,1) 
-	eps = torch.reshape(eps,(-1, d, 1)).to(device)                      # reshapes eps dim(2) -> 2x1 vector 
+	eps = torch.reshape(eps,(-1, d, 1))                     # reshapes eps dim(2) -> 2x1 vector 
 	x_noisy = x + torch.squeeze(torch.bmm(cov_matrix, eps))            # x + sigma * eps
 	loss = - torch.logsumexp(torch.reshape(model.log_prob(x_noisy),(-1, n)),dim=-1) 
 	loss +=  torch.log(torch.tensor(1.0 if not n else n))
 	return loss
 
+# def deconv_loss(model, batch, args):
+	
+# 	d = args.dim
+# 	n = args.num_mc
+
+# 	batch = batch.repeat_interleave(n, 0)#                   # ABC... -> AABBCC...
+# 	batch = GaiaTransform(batch)
+
+# 	x = batch.xv	
+# 													          # x = (x1, x2)
+# 	cov_flat = batch.cov 									  # cov = (c11, c12, c21, c22)  flat
+# 	cov_matrix = torch.reshape(cov_flat, (-1, d, d))#         # cov = ((c11, c12),(c21, c22))  reshaped
+# 	eps = torch.randn_like(x)                                 # sample eps ~ N(0,1) 
+# 	eps = torch.reshape(eps,(-1, d, 1))                       # reshapes eps dim(2) -> 2x1 vector 
+# 	x_noisy = x + torch.squeeze(torch.bmm(cov_matrix, eps))            # x + sigma * eps
+# 	loss = - torch.logsumexp(torch.reshape(model.log_prob(x_noisy),(-1, n)),dim=-1) 
+# 	loss +=  torch.log(torch.tensor(1.0 if not n else n))
+# 	return loss
+
 
 def neglogprob_joint_loss(model, batch, args):
 	d = args.dim
 	n = args.num_mc
-	device = args.device
 	x = batch[:, :d]													# x = (x1, x2)
 	cov_flat = batch[:, d:]   											# (c11, c12, c21, c22)
 	cov_flat = torch.cat((cov_flat[:, :1], cov_flat[:, -1:]), dim=1 )     # keep diagonal elements only: (c11, c22)
@@ -46,14 +63,13 @@ def neglogprob_joint_loss(model, batch, args):
 def deconv_joint_loss(model, batch, args):
 	d = args.dim
 	n = args.num_mc
-	device = args.device
-	batch = batch.repeat_interleave(n,0).to(device)                   # ABC... -> AAABBBCCC...
-	x = batch[:, :d]												  # x = (x1, x2)
-	cov_flat = batch[:, d:]   									      # (c11, c12, c21, c22)
-	cov_matrix = torch.reshape(cov_flat, (-1, d, d)).to(device)       # reshapes ((c11, c12),(c21, c22))
+	batch = batch.repeat_interleave(n,0)                               # ABC... -> AAABBBCCC...
+	x = batch[:, :d]												   # x = (x1, x2)
+	cov_flat = batch[:, d:]   									       # (c11, c12, c21, c22)
+	cov_matrix = torch.reshape(cov_flat, (-1, d, d))                   # reshapes ((c11, c12),(c21, c22))
 	cov_flat = torch.cat((cov_flat[:,:1], cov_flat[:,-1:]), dim=1 )    # keep diagonal elements only: (c11, c22)
 	eps = torch.randn_like(x)                                         # sample eps ~ N(0,1) 
-	eps = torch.reshape(eps,(-1, d, 1)).to(device)                      # reshapes eps dim(2) -> 2x1 vector 
+	eps = torch.reshape(eps,(-1, d, 1))                               # reshapes eps dim(2) -> 2x1 vector 
 	x_noisy = x + torch.squeeze(torch.bmm(cov_matrix, eps))           # data + cov*eps
 	x_covs_noisy = torch.cat((x_noisy, cov_flat), dim=1)              # (x1, x2, c11, c22)       
 	loss = - torch.logsumexp(torch.reshape(model.log_prob(x_covs_noisy),(-1, n)),dim=-1)

source/gaia_deconvolution/models/training.py
@@ -50,19 +50,27 @@ class Train_Epoch(nn.Module):
         self.args = args
 
     def fit(self, data, optimizer):
+        
         self.model.train()
         self.loss = 0
+
         for batch in tqdm(data, desc="batch"):
-            if self.args.batch_steps <= 1: 
+
+            if self.args.sub_batch_size <= 1: 
+
+                batch.to(self.args.device)
                 current_loss = calculate_loss(self.model, batch, self.args)
                 current_loss.backward()
                 optimizer.step()  
                 optimizer.zero_grad()
-                self.loss += current_loss.item() / len(data) 
-            else: # sub-batch and accumulate gradient (use if data does not fit in GPU memory)  
-                sub_batches = torch.tensor_split(batch, batch.shape[0] // self.args.batch_steps)
+                self.loss += current_loss.item() / len(data)
+
+            else: 
+                # sub-batch and accumulate gradient (use if data does not fit in GPU memory)  
+                sub_batches = torch.tensor_split(batch, batch.shape[0] // self.args.sub_batch_size)
                 sub_batch_loss = 0
                 for sub_batch in sub_batches:
+                    sub_batch.to(self.args.device)
                     current_loss = calculate_loss(self.model, sub_batch, self.args, reduction=torch.sum)
                     current_loss.backward()
                     sub_batch_loss += current_loss.item() / self.args.batch_size
@@ -91,11 +99,14 @@ class Evaluate_Epoch(nn.Module):
         self.loss = 0
         self.epoch += 1
         for batch in data:
-            if self.args.batch_steps <= 1:   
+
+            batch.to(args.device)
+
+            if self.args.sub_batch_size <= 1:   
                 current_loss = calculate_loss(self.model, batch, self.args)
                 self.loss += current_loss.item() / len(data)
             else:
-                sub_batches = torch.tensor_split(batch, batch.shape[0] // self.args.batch_steps)
+                sub_batches = torch.tensor_split(batch, batch.shape[0] // self.args.sub_batch_size)
                 sub_batch_loss = 0
                 for sub_batch in sub_batches:
                     current_loss = calculate_loss(self.model, sub_batch, self.args, reduction=torch.sum) 